834fd931f385dbc7476f7f4d19a045d287bb2302
Adam Berg <adam@xyzdigital.com>
2023-03-01 20:19:33 -0500

    Add simple push notification client side

---
+++ simple/index.js
@@ -0,0 +1,27 @@
+async function run() {
+  // A service worker must be registered in order to send notifications on iOS
+  const registration = await navigator.serviceWorker.register(
+    "serviceworker.js",
+    {
+      scope: "./",
+    }
+  );
+
+  const button = document.getElementById("subscribe");
+  button.addEventListener("click", async () => {
+    // Triggers popup to request access to send notifications
+    const result = await window.Notification.requestPermission();
+
+    // If the user rejects the permission result will be "denied"
+    if (result === "granted") {
+      // You must use the service worker notification to show the notification
+      // Using new Notification("Hello World", { body: "My first notification on iOS"}) does not work on iOS
+      // despite working on other platforms
+      await registration.showNotification("Hello World", {
+        body: "My first notification on iOS",
+      });
+    }
+  });
+}
+
+run();
