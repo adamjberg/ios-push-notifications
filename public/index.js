21a35e8744d942489357694deecdbde99bddbf06
Edmond Chui <chui.hinwai@gmail.com>
2023-04-13 01:26:30 +0100

    Use string for API key reg
    web-push supports using string directly: https://github.com/web-push-libs/web-push#using-vapid-key-for-applicationserverkey

--- public/index.js
+++ public/index.js
@@ -1,16 +1,3 @@
-function urlBase64ToUint8Array(base64String) {
-  const padding = "=".repeat((4 - (base64String.length % 4)) % 4);
-  const base64 = (base64String + padding)
-    .replace(/\-/g, "+")
-    .replace(/_/g, "/");
-  const rawData = atob(base64);
-  const outputArray = new Uint8Array(rawData.length);
-  for (let i = 0; i < rawData.length; ++i) {
-    outputArray[i] = rawData.charCodeAt(i);
-  }
-  return outputArray;
-}
-
 async function run() {
   // A service worker must be registered in order to send notifications on iOS
   const registration = await navigator.serviceWorker.register(
@@ -28,9 +15,7 @@ async function run() {
     // If the user rejects the permission result will be "denied"
     if (result === "granted") {
       const subscription = await registration.pushManager.subscribe({
-        applicationServerKey: urlBase64ToUint8Array(
-          "[[INSERT YOUR PUBLIC VAPID KEY HERE]]"
-        ),
+        applicationServerKey: "[[INSERT YOUR PUBLIC VAPID KEY HERE]]",
         userVisibleOnly: true,
       });
 
