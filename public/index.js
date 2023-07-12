4cfb17b481c7238e82a81b01762cdf77926b488c
Adam <adam@xyzdigital.com>
2023-07-12 15:11:14 -0700

    Update button to send notification after registering

--- public/index.js
+++ public/index.js
@@ -8,26 +8,40 @@ async function run() {
   );
 
   const button = document.getElementById("subscribe");
-  button.addEventListener("click", async () => {
-    // Triggers popup to request access to send notifications
-    const result = await window.Notification.requestPermission();
 
-    // If the user rejects the permission result will be "denied"
-    if (result === "granted") {
-      const subscription = await registration.pushManager.subscribe({
-        applicationServerKey: "[[INSERT YOUR PUBLIC VAPID KEY HERE]]",
-        userVisibleOnly: true,
-      });
+  const areNotificationsGranted = window.Notification.permission === "granted";
+  if (areNotificationsGranted) {
+    button.innerText = "Send Notification";
 
-      await fetch("/save-subscription", {
-        method: "post",
-        headers: {
-          "Content-Type": "application/json",
-        },
-        body: JSON.stringify(subscription),
-      });
-    }
-  });
+    button.addEventListener("click", async () => {
+      await fetch("/send-notification");
+    });
+  } else {
+    button.addEventListener("click", async () => {
+      // Triggers popup to request access to send notifications
+      const result = await window.Notification.requestPermission();
+
+      // If the user rejects the permission result will be "denied"
+      if (result === "granted") {
+        const subscription = await registration.pushManager.subscribe({
+          // TODO: Replace with your public vapid key
+          applicationServerKey:
+            "BFkG2HKrQ3BYTS_4z1S1pRwNoX4vvQhCwi3q9Hum7nQ8p9FHU3nLAjzmGWet_63jkLD2XXFp2rgranujXvCJd4k",
+          userVisibleOnly: true,
+        });
+
+        await fetch("/save-subscription", {
+          method: "post",
+          headers: {
+            "Content-Type": "application/json",
+          },
+          body: JSON.stringify(subscription),
+        });
+
+        window.location.reload();
+      }
+    });
+  }
 }
 
 run();
